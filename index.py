7ead308032884297c54a755f41f289f27db5804b
Damon <damon.yin@runwaygo.net>
2020-04-13 15:34:31 +0800

    通过参数weburl下载页面中的所有jpg和png格式图片（python 3.7）

--- index.py
+++ index.py
@@ -3,29 +3,22 @@ import sys
 import urllib
 import os
 import requests
+import hashlib
 
-
-def getPage(keyword, page, n):
-    page = page * n
-    keyword = urllib.parse.quote(keyword, safe='/')
-    url_begin = "http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word="
-    url = url_begin + keyword + "&pn=" + str(page) + "&gsm=" + str(hex(page)) + "&ct=&ic=0&lm=-1&width=0&height=0"
-    return url
-
-def get_onepage_urls(onepageurl):
+def get_pic_urls(onepageurl):
     try:
         html = requests.get(onepageurl).text
+        reg = r'http[s]{0,}://[^\s]*.jpg|http[s]{0,}://[^\s]*.png' 
+        imgre = re.compile(reg)  
+        return re.findall(imgre, html)
     except Exception as e:
-        print(e)
+        print('出错了 ❌ ', e)
         pic_urls = []
         return pic_urls
-    pic_urls = re.findall('"objURL":"(.*?)",', html, re.S)
-    return pic_urls
 
-
-def down_pic(floderName, pic_urls):
+def down_pic(imageFloder, pic_urls):
     """给出图片链接列表, 下载所有图片"""
-    path = os.getcwd() + '/' + floderName
+    path = os.getcwd() + '/' + imageFloder
     folder = os.path.exists(path)
     if not folder:
         os.makedirs(path)
@@ -34,8 +27,9 @@ def down_pic(floderName, pic_urls):
         print('文件夹已存在，之前下载的图片将被覆盖')
     for i, pic_url in enumerate(pic_urls):
         try:
+            fileName = pic_url.split('/')[-1]
             pic = requests.get(pic_url, timeout=15)
-            string = path + '/' + str(i + 1) + '.jpg'
+            string = path + '/' + fileName
             with open(string, 'wb') as f:
                 f.write(pic.content)
                 print('成功下载第%s张图片: %s' % (str(i + 1), str(pic_url)))
@@ -44,21 +38,26 @@ def down_pic(floderName, pic_urls):
             print(e)
             continue
 
-
 if __name__ == '__main__':
-    keyword = '狗'  # 关键词, 改为你想输入的词即可, 相当于在百度图片里搜索一样
-    page_begin = 0
-    page_number = 30
-    image_number = 10
-    all_pic_urls = []
-    while 1:
-        if page_begin > image_number:
-            break
-        print("第{}次请求数据".format(page_begin))
-        url = getPage(keyword, page_begin, page_number)
-        onepage_urls = get_onepage_urls(url)
-        page_begin += 1
-
-        all_pic_urls.extend(onepage_urls)
-        # set 过滤重复项
-    down_pic(keyword, list(set(all_pic_urls)))
+    argvLen = len(sys.argv)
+    if argvLen > 1 :
+        all_pic_urls = []
+        url = sys.argv[1]
+        preHeaders = url.split('/')
+        floderName = ""
+        floderNameMD5 = hashlib.md5(url.encode('utf-8')).hexdigest()
+        if len(preHeaders) > 3 :
+            floderName = preHeaders[2] + '-' + floderNameMD5
+        else:
+            floderName = floderNameMD5
+        print(floderName)
+        pic_urls = get_pic_urls(url)
+        if len(pic_urls) > 0 :
+            all_pic_urls.extend(pic_urls)
+            down_pic(floderName, list(set(all_pic_urls)))
+        else:
+            print('❌ 没有检测到图片')
+    else:
+        print('❌ 缺少参数：web url')
+        exit(1)
+    
